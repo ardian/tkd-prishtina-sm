1a069113f60753012b4b56061c3fcf6ef75aa50d
Ardian Haxha <ardian@brainpress.org>
2016-12-07 21:23:54 +0100

    PEP8 fix

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -9,7 +10,6 @@ def hello():
 @app.route("/dashboard/")
 def dashboard():
     return render_template('dashboard.html')
-
 
 
 if __name__ == "__main__":
